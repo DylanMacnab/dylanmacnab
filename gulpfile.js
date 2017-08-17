bf7be84678d84781778389ac98139b148626a79d
Dylan_Macnab <dylanmacnab@gmail.com>
2017-08-16 23:58:36 -0700

    added gulp and babel to workflow

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// Gulp.js configuration
+
+var
+  // modules
+  gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  pump = require('pump'),
+  cssnano = require('gulp-cssnano'),
+  sourcemaps = require('gulp-sourcemaps'),
+  sass = require('gulp-sass'),
+  babel = require("gulp-babel");
+
+
+  // development mode?
+  devBuild = (process.env.NODE_ENV !== 'production'),
+
+  // folders
+  folder = {
+    src: 'src/',
+    build: 'build/'
+  }
+
+  // JS Processing
+  gulp.task('js', function (cb) {
+    pump([
+          gulp.src('js/**/*'),
+          babel(),
+          uglify(),
+          gulp.dest('./js/min')
+      ],
+      cb
+    );
+  });
+
+  // SASS/SCSS Processing
+  gulp.task('scss', function() {
+    return gulp.src('scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./css'));
+  });
+
+  // Automated Tasks //
+  // *****************
+
+  // run all tasks
+  gulp.task('run', ['scss', 'js']);
+  // watch for changes
+  gulp.task('watch', function() {
+    // js
+    gulp.watch(folder.src + 'js/**/*', ['js']);
+    // sass
+    gulp.watch(folder.src + 'scss/**/*', ['scss']);
+  });
+
+  // Default Task //
+  // **************
+
+  gulp.task('default', ['run', 'watch']);
+
+;
